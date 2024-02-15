ce4bbb30decbf69b5956dda96e61425cd921c7f5
Andrew Todd <andrewdanieltodd@gmail.com>
2024-02-15 02:32:00 -0700

    Added the files for the lab

---
+++ script.js
@@ -0,0 +1,16 @@
+// Your solution goes here 
+function isStrongPassword(password) {
+    if(password.length < 8) {
+        return false;
+    }
+
+    if(password.toLowerCase().includes("password")) {
+        return false;
+    }
+
+    if(!/[A-Z]/.test(password)) {
+        return false;
+    }
+
+    return true;
+}
